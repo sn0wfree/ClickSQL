a41b06fe04b36831557c2a1d7f0f10d544c9d1f9
sn0wfree <snowfreedom0815@gmail.com>
2020-11-11 13:09:21 +0800

    alter .github/workflows/python-package.yml

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -4,6 +4,6 @@
 from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 
-__version__ = '0.1.5.4'
+__version__ = '0.1.5.5'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt']
