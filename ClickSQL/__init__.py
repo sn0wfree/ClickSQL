752729d6ac5aefc53a59012eba6f99967cb34775
sn0wfree <snowfreedom0815@gmail.com>
2020-11-23 17:46:54 +0800

    new version

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.factor_node import BaseSingleFactorTableNode
 
-__version__ = '0.1.7.3'
+__version__ = '0.1.7.4'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
