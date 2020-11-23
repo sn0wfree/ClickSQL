d6846e100b005daaca2d702ca2b6c4d69a88283c
sn0wfree <snowfreedom0815@gmail.com>
2020-11-23 16:29:22 +0800

    turn to wheel packup and alter version code as 0.1.7.2

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.factor_node import BaseSingleFactorTableNode
 
-__version__ = '0.1.7.1'
+__version__ = '0.1.7.2'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
