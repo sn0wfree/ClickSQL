eb0135d771618bc5c7c349152abde30aa202ee70
sn0wfree <snowfreedom0815@gmail.com>
2020-12-02 17:55:28 +0800

    add new raise error to avoid missing data

--- ClickSQL/eventstudy/es_model.py
+++ ClickSQL/eventstudy/es_model.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class MarketModel(DefaultModel):
     @staticmethod
-    def cal_ar(estimation_df, event_df, stock: str, formula: str = "{stock} ~ 1 + {factors}", factors=['Mkt_RF']):
+    def cal_ar(estimation_df, event_df, stock: str, formula: str = "{stock} ~ 1 + {factors}", factors=['Mkt_RF'],raise_error=False):
         """
 
         :param estimation_df:  pd.DataFrame
@@ -20,14 +20,23 @@ class MarketModel(DefaultModel):
         """
         f1 = formula.format(stock=stock, factors="+".join(factors))
         # print(f1)
-        models = sm.OLS.from_formula(f1, data=estimation_df).fit()
-        params = models.params
-        beta_Mkt = params[factors]
-        alpha = params["Intercept"]
-        bse = models.bse
-        event_df[f'{stock}_er'] = np.dot(event_df[factors].values, beta_Mkt) + alpha
-        # event_df[f'{stock}_ar'] = event_df.eval(f'{stock} - {stock}_er')
-        return event_df.eval(f'{stock} - {stock}_er')
+        if estimation_df.dropna().shape[0] == estimation_df.shape[0]:
+            models = sm.OLS.from_formula(f1, data=estimation_df).fit()
+            params = models.params
+            beta_Mkt = params[factors]
+            alpha = params["Intercept"]
+            bse = models.bse
+            event_df[f'{stock}_er'] = np.dot(event_df[factors].values, beta_Mkt) + alpha
+            # event_df[f'{stock}_ar'] = event_df.eval(f'{stock} - {stock}_er')
+            return event_df.eval(f'{stock} - {stock}_er')
+        else:
+            if raise_error:
+                raise ValueError('estimation_df has nan data!')
+            else:
+
+                series = pd.Series([None] * event_df.shape[0])
+                series.index = event_df.index
+                return series
 
 
 if __name__ == '__main__':
