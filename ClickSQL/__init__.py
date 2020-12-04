21f9b6634f00a3c198ea84c9b5cc11e8d7fc89f7
sn0wfree <snowfreedom0815@gmail.com>
2020-12-04 16:01:08 +0800

    alter cal_ar_single to cal_ar_multi

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.base import BaseSingleFactorTableNode
 
-__version__ = '0.1.7.8'
+__version__ = '0.1.7.9'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
