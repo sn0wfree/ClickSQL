d4db6121d905700ac9a927207bb59af1378e4142
sn0wfree <snowfreedom0815@gmail.com>
2020-12-06 16:27:41 +0800

    remove event study package into new repo: https://github.com/sn0wfree/EventStudySuite

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.base import BaseSingleFactorTableNode
 
-__version__ = '0.1.7.10'
+__version__ = '0.1.8.1'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
