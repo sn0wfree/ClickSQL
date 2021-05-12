42906c3e17e0cf5de31ee6c0ad13d89c097fd52b
sn0wfree <snowfreedom0815@gmail.com>
2021-05-12 13:05:08 +0800

    ft

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.base import BaseSingleFactorTableNode
 
-__version__ = '0.1.8.9.3'
+__version__ = '0.1.8.9.4'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
