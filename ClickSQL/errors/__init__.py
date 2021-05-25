27aa594785a1317803778c93b3456d432ceb59c4
sn0wfree <snowfreedom0815@gmail.com>
2021-05-25 13:20:59 +0800

    add error parse function which link to https://github.com/sn0wfree/ClickSQL/issues/8

--- ClickSQL/errors/__init__.py
+++ ClickSQL/errors/__init__.py
@@ -1,4 +1,17 @@
 # coding=utf-8
+import re
+
+ERROR_PATTERNS = (
+    # ClickHouse prior to v19.3.3
+    re.compile(r'''Code:\ (?P<code>\d+),
+            \ e\.displayText\(\)\ =\ (?P<type1>[^ \n]+):\ (?P<msg>.+?),
+            \ e.what\(\)\ =\ (?P<type2>[^ \n]+)
+        ''', re.VERBOSE | re.DOTALL),
+    # ClickHouse v19.3.3+
+    re.compile(r'''Code:\ (?P<code>\d+),
+            \ e\.displayText\(\)\ =\ (?P<type1>[^ \n]+):\ (?P<msg>.+)
+        ''', re.VERBOSE | re.DOTALL),
+)
 
 
 class ParameterKeyError(Exception):
@@ -17,7 +30,46 @@ class DatabaseNotExists(Exception): pass
 
 
 class DatabaseError(Exception):
+    '''
+    Raised when a database operation fails.
+    '''
     pass
+
+
+class ServerError(DatabaseError):
+    """
+    Raised when a server returns an error.
+    """
+
+    def __init__(self, message):
+        self.code = None
+        processed = self.get_error_code_msg(message.decode())
+        if processed:
+            self.code, self.message = processed
+        else:
+            # just skip custom init
+            # if non-standard message format
+            self.message = message
+            super(ServerError, self).__init__(message)
+
+    @classmethod
+    def get_error_code_msg(cls, full_error_message):
+        """
+        Extract the code and message of the exception that clickhouse-server generated.
+        See the list of error codes here:
+        https://github.com/yandex/ClickHouse/blob/master/dbms/src/Common/ErrorCodes.cpp
+        """
+        for pattern in ERROR_PATTERNS:
+            match = pattern.match(full_error_message)
+            if match:
+                # assert match.group('type1') == match.group('type2')
+                return int(match.group('code')), match.group('msg').strip()
+
+        return 0, full_error_message
+
+    def __str__(self):
+        if self.code is not None:
+            return "{} ({})".format(self.message, self.code)
 
 
 class HeartbeatCheckFailure(Exception):
