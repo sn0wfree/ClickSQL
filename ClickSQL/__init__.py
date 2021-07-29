e56d3e055e138b9497283fc82eb7a3d9f958d75c
sn0wfree <linlu1234567@sina.com>
2021-07-29 17:38:24 +0800

    add new function for pooled connection for clickhouse!

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.base import BaseSingleFactorTableNode
 
-__version__ = '0.1.8.9.6'
+__version__ = '0.1.9.1'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
