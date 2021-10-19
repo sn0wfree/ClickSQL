392a6fa904c3ad6a2c6ebc50cfcfd794b73051eb
sn0wfree <linlu1234567@sina.com>
2021-10-19 16:19:52 +0800

    update version
    update for debug issue：Code 516,"it is not allowed to use Authorization HTTP header and authentication via parameters simultaneously"

--- ClickSQL/__init__.py
+++ ClickSQL/__init__.py
@@ -5,6 +5,6 @@ from ClickSQL.clickhouse.ClickHouse import ClickHouseTableNode
 from ClickSQL.clickhouse.ClickHouseExt import ClickHouseTableNodeExt
 from ClickSQL.nodes.base import BaseSingleFactorTableNode
 
-__version__ = '0.1.9.2'
+__version__ = '0.1.9.3'
 __author__ = 'sn0wfree'
 __all__ = ['ClickHouseTableNode', 'ClickHouseTableNodeExt', 'BaseSingleFactorTableNode']
